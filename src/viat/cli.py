ff130394a64c16dd60b07d93e3dc837fa92d0871
Ianis Vasilev <ianis@ivasilev.net>
2026-05-12 07:54:50 +0300

    Remove obsolete module check for click

--- src/viat/cli.py
+++ src/viat/cli.py
@@ -1,14 +1,5 @@
 """The command-line interface for viat."""
 
-import importlib.util
-
-from viat.exceptions import ViatIntegrityError
-
-
-if not importlib.util.find_spec('click'):
-    raise ViatIntegrityError('Could not import the click module')
-
-
 from ._cli import viat
 
 
